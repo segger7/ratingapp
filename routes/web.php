902c01001e8e36eb379d166bfd8820ee9df56ddb
Emil <emweiss@tsn.at>
2026-06-24 10:07:51 +0200

    in web.php bei criterion edit und update hinzugefügt und den anderen routes Namen gegeben

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,28 @@ Route::inertia('/login', 'auth/login')->name('login');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     //Route::inertia('dashboard', 'dashboard')->name('dashboard');
-    Route::get('/item', [ItemController::class, 'index']);
-    Route::get('/item/create', [ItemController::class, 'create']);
-    Route::post('/item', [ItemController::class, 'store']);
-    Route::get('/item/{item}', [ItemController::class, 'show']);
-    Route::get('/item/{item}/rate', [RatingController::class, 'create']);
-    Route::post('/item/{item}/rate', [RatingController::class, 'store']);
-    Route::get('/criterion', [CriterionController::class, 'index']);
-    Route::get('/criterion/create', [CriterionController::class, 'create']);
-    Route::post('/criterion', [CriterionController::class, 'store']);
-    Route::delete('/criterion/{criterion}', [CriterionController::class, 'destroy']);
+// Item
+    Route::get('/item', [ItemController::class, 'index'])->name('item.index');
+    Route::get('/item/create', [ItemController::class, 'create'])->name('item.create');
+    Route::post('/item', [ItemController::class, 'store'])->name('item.store');
+    Route::get('/item/{item}', [ItemController::class, 'show'])->name('item.show');
+    Route::get('/item/{item}/edit', [ItemController::class, 'edit'])->name('item.edit');
+    Route::put('/item/{item}', [ItemController::class, 'update'])->name('item.update');
+    Route::delete('/item/{item}', [ItemController::class, 'destroy'])->name('item.destroy');
+
+// Rating
+    Route::get('/item/{item}/rate', [RatingController::class, 'create'])->name('rating.create');
+    Route::post('/item/{item}/rate', [RatingController::class, 'store'])->name('rating.store');
+
+// Criterion
+    Route::get('/criterion', [CriterionController::class, 'index'])->name('criterion.index');
+    Route::get('/criterion/create', [CriterionController::class, 'create'])->name('criterion.create');
+    Route::post('/criterion', [CriterionController::class, 'store'])->name('criterion.store');
+    Route::delete('/criterion/{criterion}', [CriterionController::class, 'destroy'])->name('criterion.destroy');
+    Route::get('/criterion/{criterion}/edit', [CriterionController::class, 'edit'])->name('criterion.edit');
+    Route::put('/criterion/{criterion}', [CriterionController::class, 'update'])->name('criterion.update');
+
+// Category
     Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
     Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
     Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
